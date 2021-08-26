bbd8a7606ced8ac1e3599949194bc1edf742dd16
David Veron <davidcuevas15@hotmail.com>
2021-08-26 19:24:46 -0400

    Se añade modulo de gestión de proyectos en modo prueba

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from proyecto.models import Proyecto
 
-# admin.site.register()
+admin.site.register(Proyecto)
---
+++ proyecto/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+
+from proyecto.models import Proyecto
+
+
+class ProyectoForm(ModelForm):
+    #tarea = models.CharField(max_length=100)
+    class Meta:
+        model = Proyecto #asosiciar un modelo a Proyecto
+        fields = ['nombre_proyecto', 'miembros','estado', 'fecha_inicio']
+        # widgets = {
+        #     'first_name': TextInput(
+        #         attrs={
+        #             'placeholder': 'Ingrese sus nombres',
+        #         }
+        # exclude = ['first_name', 'last_name', 'email', 'username', 'password','groups', 'user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff']
+class ProyectoCrearForms(ModelForm):
+    class Meta:
+        model = Proyecto
+        fields =  ['nombre_proyecto','estado', 'fecha_inicio']
