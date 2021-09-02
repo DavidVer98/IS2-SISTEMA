7e0297fbc584ea41d40408fa95c0a5badce87447
adams <nicholasjara@fpuna.edu.py>
2021-09-01 20:01:27 -0400

    Modificaciones en modulo de Proyecto

--- proyecto/forms.py
+++ proyecto/forms.py
@@ -1,7 +1,20 @@
+from django import forms
+from django.contrib.contenttypes.models import ContentType
 from django.forms import ModelForm
+from django.contrib.auth.models import Group, Permission
+from proyecto.models import Proyecto, Miembro
 
-from proyecto.models import Proyecto
-
+permissions = [
+    ("VISUALIZAR_PROYECTOS", "Puede visualizar los proyectos creados"),
+    ("CREAR_PROYECTOS", "Puede crear proyectos"),
+    ("CAMBIAR_ESTADO_PROYECTO", "Puede cambiar el estado de proyectos"),
+    ("ELIMINAR_PROYECTOS", "Puede elimiar proyectos"),
+    ("AGREGAR_MIEMBRO", "Puede agregar miembros a un proyecto"),
+    ("ELIMINAR_MIEMBRO", "Puede eliminar miembros de un proyecto"),
+    ("VISUALIZAR_ROLES", "Puede ver la lista de roles del proyecto"),
+    ("CREAR_ROL", "Puede ver la lista de roles del proyecto"),
+    ("ASIGNAR_ROL", "Puede asignar rol a un miembro de proyecto"),
+]
 
 class ProyectoForm(ModelForm):
     #tarea = models.CharField(max_length=100)
@@ -18,3 +31,15 @@ class ProyectoCrearForms(ModelForm):
     class Meta:
         model = Proyecto
         fields =  ['nombre_proyecto','scrum_master','estado', 'fecha_inicio']
+
+class setMiembroForms(ModelForm):
+    # pk = forms.IntegerField()
+    class Meta:
+        model = Miembro
+        fields =  ['miembro','proyectos','rol']
+        # exclude = ['pk']
+
+class CrearGrupo(forms.Form):
+
+    nombre = forms.CharField(max_length=50, required=True)
+    permisos_proyecto = forms.MultipleChoiceField(choices=permissions, required=False)
--- sgp/settings_pruebas.py
+++ sgp/settings_pruebas.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-e(4p_%e+ghgyab950v#7+f_bfib^g#ek$@bx(ou3z*0$_v39f*
 DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.100.3', '127.0.0.1']
-
 
 # Application definition
 
@@ -49,8 +47,9 @@ INSTALLED_APPS = [
     'user',
     'home',
     'proyecto',
-    'rol',
 
+    # django-guardian
+    'guardian',
 ]
 
 MIDDLEWARE = [
@@ -84,7 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sgp.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -100,12 +98,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sgp_db_pruebas',
         'USER': 'postgres',
-        'PASSWORD' : 'admin',
-        'HOST' : 'localhost',
-        'PORT' : '5432'
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -125,7 +122,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -139,14 +135,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    )
+)
 
 # STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
@@ -171,6 +166,11 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend', # this is default
+    'guardian.backends.ObjectPermissionBackend',
+)
+
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = 'home'
