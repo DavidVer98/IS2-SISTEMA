2af027c64f2d803d5f0cc848730e9acc8188b0dd
AlexisCubilla <alexiscubilla500@fpuna.edu.py>
2021-09-02 00:04:22 -0400

    funcion de eliminar miembros de un proyecto agregado

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from proyecto.views import proyecto, getMiembros, setMiembros, crearGrupo
+from proyecto.views import proyecto, getMiembros, setMiembros, crearGrupo, eliminarmiembro
 
 urlpatterns = [
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('<int:proyecto_id>/miembros', getMiembros, name="miembros_proyecto"),
     path('<int:proyecto_id>/miembros/añadir', setMiembros, name="setMiembros_proyectos"),
     path('<int:proyecto_id>/roles/', crearGrupo, name="roles_proyecto"),
-
+    path('<int:proyecto_id>/miembros/<int:miembro_id>/eliminar', eliminarmiembro, name="eliminarmiembro_proyecto"),
     # r'^/(?P<proyecto_id>\d+)/$' setMiembros '<int:proyecto_id>/miembros' proyect/1/miembros
 
 ]
