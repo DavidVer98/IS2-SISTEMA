91db9f38e2af18042d0f94b6117534c88104e3d0
elias <eliasrecalde99@fpuna.edu.py>
2021-09-03 14:59:16 -0400

    Test unitarios

---
+++ proyecto/tests/test_urls.py
@@ -0,0 +1,48 @@
+
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse,resolve
+from proyecto.views import proyecto, getMiembros,setMiembros,crearGrupo,listarRol,editarRol,eliminarRol,eliminarmiembro,editar_rolmiembro,eliminarProyecto
+
+class TestUrls(SimpleTestCase):
+
+    def test_proyecto_url(self):
+        url=reverse('proyecto',kwargs={"proyecto_id":1})
+        self.assertEqual(resolve(url).func,proyecto)
+
+    def test_getMiembros_url(self):
+        url=reverse('miembros_proyecto',kwargs={"proyecto_id":1})
+        self.assertEqual(resolve(url).func,getMiembros)
+
+    def test_agregarMiembros_url(self):
+        url=reverse('setMiembros_proyectos',kwargs={"proyecto_id":1})
+        self.assertEqual(resolve(url).func,setMiembros)
+
+    def test_crearRol(self):
+        url=reverse('roles_proyecto',kwargs={"proyecto_id":1})
+        self.assertEqual(resolve(url).func,crearGrupo)
+
+    def test_listarRol(self):
+        url=reverse('listaRol',kwargs={"proyecto_id":1 })
+        self.assertEqual(resolve(url).func,listarRol)
+
+    def test_editarRol(self):
+        url=reverse('editarRol',kwargs={"proyecto_id":1, "rol_id":1 })
+        self.assertEqual(resolve(url).func,editarRol)
+
+    def test_eliminarRol(self):
+        url=reverse('eliminarRol',kwargs={"proyecto_id":1, "rol_id":1 })
+        self.assertEqual(resolve(url).func,eliminarRol)
+
+    def test_eliminarMiembro(self):
+        url=reverse('eliminarmiembro_proyecto',kwargs={"proyecto_id":1, "miembro_id":2 })
+        self.assertEqual(resolve(url).func,eliminarmiembro)
+
+    def test_editarMiembro(self):
+        url=reverse('editarmiembro-proyecto',kwargs={"proyecto_id":1, "miembro_id":2 })
+        self.assertEqual(resolve(url).func,editar_rolmiembro)
+
+    def test_eliminarproyecto_url(self):
+        url=reverse('eliminarProyecto',kwargs={"proyecto_id":1})
+        self.assertEqual(resolve(url).func,eliminarProyecto)
+
+
