2b0799be721f7aea04a364dfc9f8c3e0b39f98df
AlexisCubilla <alexiscubilla500@fpuna.edu.py>
2021-09-03 15:33:53 -0400

    se añade comentarios al view de user y de oauth_app

--- oauth_app/views.py
+++ oauth_app/views.py
@@ -5,10 +5,16 @@ from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def login_views (request):
+    """
+        Vista utilizada para retornar al login  03/09/21
+    """
     return render(request,'user/login.html')
 
 
 def logout_view(request):
+    """
+        Vista utilizada para cerrar sesion  03/09/21
+    """
     auth_logout(request)
     logout(request)
     return redirect('login')
