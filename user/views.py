29bafe4d38048764c51edf68bfe1755350761838
AlexisCubilla <alexiscubilla500@fpuna.edu.py>
2021-09-15 18:04:35 -0400

    se agrega funcion para enviar correo de autorizacion de nuevo usuario a los administradores

--- user/views.py
+++ user/views.py
@@ -2,9 +2,53 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from user.models import User
+
+
 @login_required(login_url='/login')
 def activar(request):
     """
         Vista desplegada a usuarios no activados  03/09/21
     """
+    msg(request.user.email)
     return render(request, 'user/activar.html')
+
+
+# import necessary packages
+from email.message import EmailMessage
+# https://www.google.com/settings/security/lesssecureapps aceptar
+# https://www.google.com/accounts/DisplayUnlockCaptcha
+
+
+# import necessary packages
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+listadeemail=[]
+def msg(email1):
+    # create message object instance
+    if email1 not in listadeemail:
+        listadeemail.append(email1)
+        usuario = User.objects.filter(rolSistema="Administrador")
+        for u in usuario:
+            msg = MIMEMultipart()
+
+            message = "el usuario con email " + email1 + " ha intentado ingresar al sistema"
+
+            # setup the parameters of the message
+            password = 'sgp12345'
+            msg['From'] = "sistemagestordeproyectos@gmail.com"
+            msg['To'] = u.email
+            msg['Subject'] = "Usuario no autorizado"
+            fromaddr = "chapiparrizo@gmail.com"
+            # add in the message body
+            msg.attach(MIMEText(message, 'plain'))
+
+            smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+            smtpserver.ehlo()
+            smtpserver.starttls()
+            smtpserver.ehlo()
+            smtpserver.login(msg['From'], password)
+            smtpserver.sendmail(msg['From'], msg['To'], msg.as_string())
+            smtpserver.quit()
