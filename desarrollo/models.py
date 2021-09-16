d866f8ee937a0c5ea02c343f144cf63faa8612f9
adams <nicholasjara1998@fpuna.edu.py>
2021-09-16 19:45:28 -0400

    Cambiado crear proyecto

--- desarrollo/models.py
+++ desarrollo/models.py
@@ -6,10 +6,9 @@ from user.models import User
 
 
 class UserStory(models.Model):
-
-    TO_DO='TO DO'
-    DOING='DOING'
-    DONE='DONE'
+    TO_DO = 'TO DO'
+    DOING = 'DOING'
+    DONE = 'DONE'
     QA = 'QA'
     RELASE = 'RELASE'
 
@@ -28,36 +27,35 @@ class UserStory(models.Model):
         (NORMAL, 'Normal'),
         (ALTA, 'Alta'),
     )
-    class Meta:
-        ordering = ["-prioridad"]
 
     EN_PRODUCT_BACKLOG = 'EN PRODUCT BACKLOG'
-    EN_SPRINT_BACKLOG='EN SPRINT BACKLOG'
-    EN_SPRINT_PLANNING='EN SPRINT PLANNING'
+    EN_SPRINT_BACKLOG = 'EN SPRINT BACKLOG'
+    EN_SPRINT_PLANNING = 'EN SPRINT PLANNING'
 
     ESTADO_DESARROLLO_USERSTORY_CHOICES = [
-        ( EN_PRODUCT_BACKLOG, 'EN PRODUCT BACKLOG'),
+        (EN_PRODUCT_BACKLOG, 'EN PRODUCT BACKLOG'),
         (EN_SPRINT_BACKLOG, 'EN SPRINT BACKLOG'),
         (EN_SPRINT_PLANNING, 'EN SPRINT PLANNING'),
     ]
 
-
-
     proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
-    miembro_asignado = models.ForeignKey(User,  on_delete=models.PROTECT,blank=True, null=True)
+    miembro_asignado = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField(max_length=300)
     estado_sprint = models.CharField(max_length=50, choices=ESTADO_USERSTORY_CHOICES, default=TO_DO)
     estimacion = models.FloatField(default=0, blank=True, null=False)
     prioridad = models.IntegerField(default=BAJA, choices=PRIORIDAD_USERSTORY_CHOICES)
-    estado_desarrollo = models.CharField(max_length=50,default=EN_PRODUCT_BACKLOG, choices=ESTADO_DESARROLLO_USERSTORY_CHOICES)
+    estado_desarrollo = models.CharField(max_length=50, default=EN_PRODUCT_BACKLOG,
+                                         choices=ESTADO_DESARROLLO_USERSTORY_CHOICES)
+
+    class Meta:
+        ordering = ["-prioridad"]
 
 
 class EstimacionPlanificada(models.Model):
     user_story = models.ForeignKey(UserStory, on_delete=models.CASCADE)
-    estimacion_scrum = models.PositiveIntegerField(default=0,blank=True, null=True)
+    estimacion_scrum = models.PositiveIntegerField(default=0, blank=True, null=True)
     estimacion_miembro = models.PositiveIntegerField(default=0, blank=True, null=True)
-
 
 # class ProductBacklog(models.Model):
 #     userStories = models.ManyToManyField(UserStory)
@@ -67,9 +65,3 @@ class EstimacionPlanificada(models.Model):
 #     userStories = models.ManyToManyField(UserStory)
 #     miembros = models.ManyToManyField(Miembro)
 #
-
-
-
-
-
-
