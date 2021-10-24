44cc896347373ccdf074beecf3060d39f6d0f4a0
David Veron <davidcuevas15@hotmail.com>
2021-10-24 16:19:13 -0300

    Se añade el objeto UserStory al admin de django

--- desarrollo/admin.py
+++ desarrollo/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from desarrollo.models import RegistroUserStory, Sprint
+from desarrollo.models import RegistroUserStory, Sprint, UserStory
 
 admin.site.register(RegistroUserStory)
 admin.site.register(Sprint)
+admin.site.register(UserStory)
