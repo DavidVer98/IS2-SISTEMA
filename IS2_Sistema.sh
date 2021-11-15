23b0439d94e72bbdc12ad91c156b51cc9ab7b57e
AlexisCubilla <alexiscubilla500@fpuna.edu.py>
2021-11-14 22:07:18 -0300

    script de despliegue actualizado

--- IS2_Sistema.sh
+++ IS2_Sistema.sh
@@ -4,9 +4,32 @@ git clone git@github.com:DavidVer98/IS2-SISTEMA.git
 
 cd IS2-SISTEMA
 
-TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+echo "Elegir entorno (produccion o desarrollo)"
+
+read rama
+
+echo $rama
+
+if [ "$rama" = "desarrollo" ]; then
+	echo "Elegir tag"
+	git tag | grep -v _produccion
+	read TAG
+elif [ "$rama" = "produccion" ]; then
+	echo "Elegir tag"
+	git tag | grep _produccion
+	read TAG
+else 
+	echo "ingreso una rama equivocada, por defecto estara en la rama main"
+fi
+
+#TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 
 ./tags.sh $TAG
 
-./despliegue.sh
+
+if [[ $TAG == *"5"* || $TAG == *"6"* ]]; then
+
+	./despliegue.sh
+fi
+
 
